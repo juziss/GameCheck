b880be9700b6c9655daec621f486eafbb4527098
juziss <julianeta26@gmail.com>
2024-11-01 21:04:45 -0300

    Upadate na validação de campos

--- client/js/script.js
+++ client/js/script.js
@@ -11,3 +11,63 @@ $(document).ready(function() {
         $('.logo, h1, .linha, .redefinir-senha, .input-email, .input-senha, .entrar-botao, .cadastro').show();
     });
 });
+
+$(document).ready(function() {
+    
+    $('form[action="#"]').eq(0).submit(function(event) {
+        let email = $('#email').val();
+        let senha = $('#senha').val();
+        let isValid = true;
+
+        if (!validateEmail(email)) {
+            alert("Por favor, insira um e-mail válido.");
+            isValid = false;
+        }
+
+        if (senha.length < 8) {
+            alert("A senha deve ter no mínimo 8 caracteres.");
+            isValid = false;
+        }
+
+        if (!isValid) {
+            event.preventDefault();
+        }
+    });
+
+    $('form[action="#"]').eq(1).submit(function(event) {
+        let nome = $('#nome').val();
+        let emailCadastro = $('#email-cadastro').val();
+        let senhaCadastro = $('#senha-cadastro').val();
+        let confirmacao = $('#confirmação').val();
+        let isValid = true;
+
+        if (nome.trim() === "") {
+            alert("Por favor, insira seu nome completo.");
+            isValid = false;
+        }
+
+        if (!validateEmail(emailCadastro)) {
+            alert("Por favor, insira um e-mail válido.");
+            isValid = false;
+        }
+
+        if (senhaCadastro.length < 8) {
+            alert("A senha deve ter no mínimo 8 caracteres.");
+            isValid = false;
+        }
+
+        if (senhaCadastro !== confirmacao) {
+            alert("As senhas não coincidem.");
+            isValid = false;
+        }
+
+        if (!isValid) {
+            event.preventDefault();
+        }
+    });
+
+    function validateEmail(email) {
+        let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
+});
